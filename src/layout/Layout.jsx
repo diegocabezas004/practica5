75620ac072b18c05a10cd059075f3857d219118a
diegocabezas004 <diegocabezas72@gmail.com>
2025-03-16 23:37:03 -0600

    sin el dom

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,4 +1,4 @@
-import { Outlet } from "react-router-dom";
+import { Outlet } from "react-router";
 import Notification from "../components/Notification";
 import Header from "../components/Header";
 import Modal from "../components/Modal";
